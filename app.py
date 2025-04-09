2b9d5ed69f5eaa6b14878768dc78a20876400b31
Kshitij Sharma <kshitij667@gmail.com>
2025-04-09 12:19:23 +0100

    Initial Commit

---
+++ app.py
@@ -0,0 +1,5 @@
+def hello_world(name):
+    return f"Hello, {name}!"
+    
+if __name__=="__main__":
+    print(hello_world(DevOps))
---
+++ test_app.py
@@ -0,0 +1,4 @@
+from app import hello_world
+
+def test_case():
+    assert hello_world("DevOps") == "Hello, DevOps"
